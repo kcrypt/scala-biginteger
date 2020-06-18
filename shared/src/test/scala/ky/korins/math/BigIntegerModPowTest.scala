kcrypt/scala-biginteger

commit 81821693364c23c6b2f9e404e456aa639375e382

Reduce number of applied bitmas at `monReduction`

shared/src/main/scala/ky/korins/math/Division.scala
@@ -883,26 +883,25 @@ private[math] object Division {
   private def monReduction(res: Array[Int], modulus: BigInteger, n2: Int): Unit = {
     val modulusDigits = modulus.digits
     val modulusLen = modulus.numberLength
-    var outerCarry: Int = 0 // unsigned
+    var outerCarry: Long = 0 // unsigned
     val n2u = n2 & UINT_MAX
     var i = 0
     while (i < modulusLen) {
-      var innnerCarry: Int = 0 // unsigned
-      val m = ((res(i) & UINT_MAX) * n2u).toInt & UINT_MAX
+      var innnerCarry: Long = 0 // unsigned
+      val m = ((res(i) & UINT_MAX) * n2u) & UINT_MAX
       var j = 0
       while (j < modulusLen) {
-        val nextInnnerCarry = m * (modulusDigits(j) & UINT_MAX) + (res(i + j) & UINT_MAX) + (innnerCarry & UINT_MAX)
+        val nextInnnerCarry = m * (modulusDigits(j) & UINT_MAX) + (res(i + j) & UINT_MAX) + innnerCarry
         res(i + j) = nextInnnerCarry.toInt
-        innnerCarry = (nextInnnerCarry >> 32).toInt
+        innnerCarry = nextInnnerCarry >> 32
         j += 1
       }
-      val nextOuterCarry =
-        (outerCarry & UINT_MAX) + (res(i + modulusLen) & UINT_MAX) + (innnerCarry & UINT_MAX)
+      val nextOuterCarry = outerCarry + (res(i + modulusLen) & UINT_MAX) + innnerCarry
       res(i + modulusLen) = nextOuterCarry.toInt
-      outerCarry = (nextOuterCarry >> 32).toInt
+      outerCarry = nextOuterCarry >> 32
       i += 1
     }
-    res(modulusLen << 1) = outerCarry
+    res(modulusLen << 1) = outerCarry.toInt
     var j = 0
     while (j <= modulusLen) {
       res(j) = res(j + modulusLen)

commit 01d4c5c5ef2440a3a4b57f4a59ba5452133b0039

Replaced `>>` to `>>>`
`>>` made shift for signed value when `>>>` for unsigned.

This also introduced a new unit test that covered this case.

This is a fix a bug that was introduced in
81821693364c23c6b2f9e404e456aa639375e382 at 17 June 2020.

shared/src/main/scala/ky/korins/math/Division.scala
@@ -908,12 +908,12 @@ private[math] object Division {
       while (j < modulusLen) {
         val nextInnerCarry = m * (modulusDigits(j) & UINT_MAX) + (res(i + j) & UINT_MAX) + innerCarry
         res(i + j) = nextInnerCarry.toInt
-        innerCarry = nextInnerCarry >> 32
+        innerCarry = nextInnerCarry >>> 32
         j += 1
       }
       val nextOuterCarry = outerCarry + (res(i + modulusLen) & UINT_MAX) + innerCarry
       res(i + modulusLen) = nextOuterCarry.toInt
-      outerCarry = nextOuterCarry >> 32
+      outerCarry = nextOuterCarry >>> 32
       i += 1
     }
     res(modulusLen << 1) = outerCarry.toInt

shared/src/test/scala/ky/korins/math/BigIntegerModPowTest.scala
@@ -285,4 +285,13 @@ class BigIntegerModPowTest extends wordspec.AnyWordSpec {
     }
     assert(1 == result.signum())
   }
+
+  "Overflow int at monReduction" in {
+    val that = new BigInteger("3305421065326933993902152933720599759383448872735079828046644324196784608401692")
+    val exponent = new BigInteger("1436717344494928391383870735860707930000062552532987154529176227809955095439131")
+    val modulus = new BigInteger("5746869377979713565535482943442831720005209173643665270846638168530759837688261")
+    val result = that.modPow(exponent, modulus)
+
+    assert(result == new BigInteger("5460564903238843431550363269308342873211315346397855429778722709516677044851419"))
+  }
 }