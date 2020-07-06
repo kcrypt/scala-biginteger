6ca9bd0028a63a3752db818b173e01168dfefaec
Kirill A. Korinsky <kirill@korins.ky>
2020-07-06 23:10:06 +0200

    Added ProbablePrimeBenchmark

---
+++ bench/src/main/scala/ky/korins/math/benchmark/ProbablePrimeBenchmark.scala
@@ -0,0 +1,41 @@
+package ky.korins.math.benchmark
+
+import org.openjdk.jmh.annotations._
+
+@State(Scope.Benchmark)
+class ProbablePrimeBenchmark extends BaseBenchmark {
+  @Param(Array("128", "512", "1024", "4096", "8192"))
+  override var bits: Int = 0
+
+  @Setup
+  def setup(): Unit =
+    super.setupNumbers()
+
+  @Benchmark
+  def java(): Unit = {
+    javaPrime1.nextProbablePrime()
+    javaPrime2.nextProbablePrime()
+    javaPrimeHalf.nextProbablePrime()
+  }
+
+  @Benchmark
+  def scalajs(): Unit = {
+    sPrime1.nextProbablePrime()
+    sPrime2.nextProbablePrime()
+    sPrimeHalf.nextProbablePrime()
+  }
+
+  @Benchmark
+  def java_native(): Unit = {
+    nPrime1.nextProbablePrime()
+    nPrime2.nextProbablePrime()
+    nPrimeHalf.nextProbablePrime()
+  }
+
+  @Benchmark
+  def korinsky(): Unit = {
+    kPrime1.nextProbablePrime()
+    kPrime2.nextProbablePrime()
+    kPrimeHalf.nextProbablePrime()
+  }
+}
